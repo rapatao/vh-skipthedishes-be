acf0bfa39454f714fceacc12a196e1a44832bc9e
Luiz H. Rapatao <luiz@rapatao.com>
2018-06-23 13:37:35 -0300

    feature: add reason

--- src/main/java/com/vanchack/challange/skipthedishes/services/user/UserNotExists.java
+++ src/main/java/com/vanchack/challange/skipthedishes/services/user/UserNotExists.java
@@ -3,6 +3,6 @@ package com.vanchack.challange.skipthedishes.services.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "User does not exist")
 public class UserNotExists extends RuntimeException {
 }
--- src/main/java/com/vanchack/challange/skipthedishes/services/user/UserRepository.java
+++ src/main/java/com/vanchack/challange/skipthedishes/services/user/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+interface UserRepository extends CrudRepository<User, Integer> {
 
     Optional<User> findUserByUsername(String username);
 
